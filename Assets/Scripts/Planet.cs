4fde9ff3a849887edba53bdca3dd3b34dec8a320
Ruslan Kaybyshev <rkay94@gmail.com>
2013-11-08 02:44:54 -0500

    -setting up helper libraries, starting planets

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planet : MonoBehaviour
+{
+    int population;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+}
