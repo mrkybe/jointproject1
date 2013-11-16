dd37e2e37554b6b288ad14e3133723af3d1398db
Ruslan Kaybyshev <rkay94@gmail.com>
2013-11-15 22:51:27 -0500

    -UI working

---
+++ Assets/Scripts/Classes/AttachUIRoot.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttachUIRoot : MonoBehaviour {
+
+    public Camera targetAttach;
+	// Use this for initialization
+	void Start ()
+    {
+        transform.parent = targetAttach.transform;
+        //transform.rotation = targetAttach.transform.rotation;
+        transform.position = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = Vector3.zero;
+	}
+}
