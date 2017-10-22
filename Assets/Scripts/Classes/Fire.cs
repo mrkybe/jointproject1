286f7207e21392a742af742d67b0e87e5f4756fb
Zachary Lopez <ztl25@drexel.edu>
2017-10-22 14:38:50 -0400

    Bullet
    Added basic Bullet Script

---
+++ Assets/Scripts/Classes/Fire.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+
+	public float speed = 2;
+	public GameObject ammo;
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Shoot ();
+		}
+		
+	}
+
+	void FindProjectile() {
+		
+
+
+	}
+
+	void Shoot()
+	{
+		GameObject bulletClone = Instantiate(ammo, transform.position, transform.rotation);
+		Rigidbody rb = bulletClone.GetComponent<Rigidbody>();
+		rb.velocity = transform.forward * speed;
+	}
+}
