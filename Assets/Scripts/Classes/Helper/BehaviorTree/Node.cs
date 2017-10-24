a0f0b6930383ed86245627bb64145d0835513397
rkay94 <rkay94@gmail.com>
2017-10-24 16:31:50 -0400

    -behavior tree skeleton added

---
+++ Assets/Scripts/Classes/Helper/BehaviorTree/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Classes.Helper.BehaviorTree
+{
+    abstract class Node
+    {
+        public string Name = "";
+        public List<Node> Children = new List<Node>();
+        public Node Parent = null;
+        public Node Root = null;
+
+        public Node()
+        {
+            throw new NotImplementedException();
+        }
+
+        void AddChild(Node child)
+        {
+            Children.Add(child);
+        }
+    }
+}
---
+++ Assets/Scripts/Classes/Helper/BehaviorTree/RootNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Classes.Helper.BehaviorTree
+{
+    class RootNode : Node
+    {
+        HashSet<List<string>> stacks = new HashSet<List<string>>();
+
+        public RootNode(string name)
+        {
+            Name = name;
+        }
+    }
+}
