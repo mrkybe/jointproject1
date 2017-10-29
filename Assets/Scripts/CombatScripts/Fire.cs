b5204f37a5a535661153f3f650ed1f8804ce9880
Zachary Lopez <ztl25@drexel.edu>
2017-10-29 16:35:55 -0400

    Input Manager Changes
    Added contoller movement. Fire is WIP

--- Assets/Scripts/CombatScripts/Fire.cs
+++ Assets/Scripts/CombatScripts/Fire.cs
@@ -4,24 +4,26 @@ using UnityEngine;
 
 public class Fire : MonoBehaviour {
 
-	public float speed = 10;
+	public float speed = 1;
 	public GameObject ammo;
 	private GameObject bulletClone;
+    private Rigidbody rb;
 	// Use this for initialization
 	void Start ()
 	{
 		bulletClone = new GameObject ();
+        rb = new Rigidbody();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Space)) {
+		if (Input.GetButtonDown("Fire1")) {
 			if (bulletClone == true) {
 				Destroy (bulletClone);
 			}
-			bulletClone = Shoot ();
+			Shoot ();
 		}
-		bulletClone.transform.Translate (transform.forward * Time.deltaTime * speed * -1);
+        rb.AddForce(new Vector3(transform.position.x, transform.position.y * speed, transform.position.z), ForceMode.Force);
 
 	}
 
@@ -31,10 +33,10 @@ public class Fire : MonoBehaviour {
 
 	}
 
-	GameObject Shoot()
+	void Shoot()
 	{
 		float dist  = transform.position.z + 6f;
-		GameObject clone = Instantiate(ammo, new Vector3(transform.position.x, transform.position.y, dist), transform.rotation);
-		return clone;
+		bulletClone = Instantiate(ammo, new Vector3(transform.position.x, transform.position.y, dist), transform.rotation);
+        rb = bulletClone.GetComponent<Rigidbody>();
 	}
 }
