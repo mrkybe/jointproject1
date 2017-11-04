2ccad557fb36f00c5a4181287f1a7cfa3fab2eee
Alan Tsai <alan90011@gmail.com>
2017-11-04 15:30:46 -0400

    Not sure this is the right way. Skeleton code

--- Assets/Scripts/Classes/Static/Building.cs
+++ Assets/Scripts/Classes/Static/Building.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using ShipInternals;
 
-namespace AssemblyCSharp
+public class Building : Static
 {
-	public class Building
+	[SerializeField]
+	private List<Building> _buildings;
+	private List<CargoItem> _cargos;
+
+	private CargoHold resources;
+
+	private float nextActionTime = 0.0f;
+	public float period = 1.0f;
+
+	string [] resourceType = new string[4];
+
+	void Start()
 	{
-		public Building ()
-		{
+		
+		resourceType [0] = "Iron";
+		resourceType [1] = "Steel";
+		resourceType [2] = "Food";
+		resourceType [3] = "Wool";
+
+
+	}
+
+	public void factoryProduce()
+	{
+		
+		resources.addHoldType("Iron");
+
+		_buildings.Add (resources);
+
+
+	}
+
+
+	List<CargoItem> cargoList= new List<CargoItem>();
+	void Update () {
+		if (Time.time > nextActionTime ) {
+			nextActionTime += period;
+			cargoList.AddRange (resourceType);
+
 		}
 	}
-}
 
+
+
+}
