f76b674d8e5be778ba68add3d33a9b72225fd6cf
Zachary Lopez <ztl25@drexel.edu>
2018-02-03 20:05:41 -0500

    Succesfully checks for controller

---
+++ Assets/Scripts/CombatScripts/DetectController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectController : MonoBehaviour {
+
+    public Move moveScript;
+	// Use this for initialization
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0) || Input.GetMouseButton(1) || Input.GetMouseButton(2))
+            moveScript.controller = false;
+        if (Input.GetAxisRaw("HorizontalR") != 0 || Input.GetAxisRaw("VerticalR") != 0)
+            moveScript.controller = true;
+	}
+}
--- Assets/Scripts/CombatScripts/Move.cs
+++ Assets/Scripts/CombatScripts/Move.cs
@@ -6,25 +6,21 @@ public class Move : MonoBehaviour {
 
     public float speed = 10f;
 	public float rotateSpeed = .05f;
-   
-	private Rigidbody rb;
+    public bool controller;
+
+    private Rigidbody rb;
 	private Vector3 lookPos;
-	private bool mouse;
-    private bool controller;
     private Vector3 velocity;
     // Use this for initialization
     void Start ()
     {
         rb = gameObject.GetComponent<Rigidbody>();
-        mouse = true;
     }
 
 	void Update()
 	{
 		timeMove ();
-		mouseLook ();
-        stickLook();
-        checkCont();
+        look();
     }
 	// Update is called once per frame
 	void FixedUpdate ()
@@ -43,50 +39,44 @@ public class Move : MonoBehaviour {
         velocity = move * speed;
 	}
 
-	//movement based on forces, requires and FixedUpdate
-	void forceMove()
-	{
-		float x = Input.GetAxis ("Horizontal");
-		float y = Input.GetAxis("Vertical");
-
-		Vector3 movement = new Vector3(x,0,y);
-		rb.AddForce (movement * speed / Time.deltaTime);
-	}
-
-	// rays to use mouse for rotation, requires Update
-	void mouseLook()
-	{
-		if (mouse) 
-		{
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			Plane ground = new Plane (Vector3.up, Vector3.zero);
-			float raylength;
-
-			if (ground.Raycast (ray, out raylength)) 
-			{
-				Vector3 pointToLook = ray.GetPoint (raylength);
-				Debug.DrawLine (ray.origin, pointToLook, Color.magenta);
-				transform.LookAt (new Vector3 (pointToLook.x, transform.position.y, pointToLook.z));
-			}
-		}
-	}
-
-    void stickLook()
+    //movement based on forces, requires and FixedUpdate
+    void forceMove()
     {
-        float rx = Input.GetAxis("HorizontalR");
-        float rz = Input.GetAxis("VerticalR");
-        Vector3 direction = Vector3.right * rx + Vector3.forward * rz;
-        if(direction.sqrMagnitude > 0)
-        {
-            transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
-        }
-        //float angle = Mathf.Atan2 (rx, rz);
-        //transform.rotation = Quaternion.Euler(0, angle * Mathf.Rad2Deg, 0);
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(x, 0, y);
+        rb.AddForce(movement * speed / Time.deltaTime);
     }
 
-	void checkCont()
-	{
-		Debug.Log (Input.GetJoystickNames());
-	}
+    void look()
+    {
+        // if there is no controller, follow the mouse
+        // else if there is a controller, use the sticks to
+        // calculate direction
+        if (!controller)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Plane ground = new Plane(Vector3.up, Vector3.zero);
+            float raylength;
+
+            if (ground.Raycast(ray, out raylength))
+            {
+                Vector3 pointToLook = ray.GetPoint(raylength);
+                Debug.DrawLine(ray.origin, pointToLook, Color.magenta);
+                transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+            }
+        }
+        if(controller)
+        {
+            float rx = Input.GetAxis("HorizontalR");
+            float rz = Input.GetAxis("VerticalR");
+            Vector3 direction = Vector3.right * rx + Vector3.forward * rz;
+            if (direction.sqrMagnitude > 0)
+            {
+                transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
+            }
+        }
+    }
 		
 }
