641b795b860188d2618a3d804ed748618deaaf95
Zachary Lopez <ztl25@drexel.edu>
2018-02-06 18:09:05 -0500

    Updated some scripts

--- Assets/Scripts/CombatScripts/Move.cs
+++ Assets/Scripts/CombatScripts/Move.cs
@@ -15,7 +15,6 @@ public class Move : MonoBehaviour {
     void Start ()
     {
         rb = gameObject.GetComponent<Rigidbody>();
-		mouse = true;
     }
 
 	void Update()
@@ -25,8 +24,9 @@ public class Move : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate ()
     {
-		rb.velocity = velocity;
+		
         look();
+		rb.velocity = velocity;
     }
 
 
@@ -46,44 +46,27 @@ public class Move : MonoBehaviour {
 		transform.rotation = Quaternion.EulerAngles (0,angle * rotateSpeed,0);
         velocity = move * speed;
 	}
-
-    //movement based on forces, requires and FixedUpdate
-    void forceMove()
-    {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(x, 0, y);
-        rb.AddForce(movement * speed / Time.deltaTime);
-    }
-
     void look()
     {
         // if there is no controller, follow the mouse
         // else if there is a controller, use the sticks to
         // calculate direction
-        if (!controller)
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Plane ground = new Plane(Vector3.up, Vector3.zero);
-            float raylength;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane ground = new Plane(Vector3.up, Vector3.zero);
+        float raylength;
 
-            if (ground.Raycast(ray, out raylength))
-            {
-                Vector3 pointToLook = ray.GetPoint(raylength);
-                Debug.DrawLine(ray.origin, pointToLook, Color.magenta);
-                transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
-            }
-        }
-        if(controller)
+        if (ground.Raycast(ray, out raylength))
         {
-            float rx = Input.GetAxis("HorizontalR");
-            float rz = Input.GetAxis("VerticalR");
-            Vector3 direction = Vector3.right * rx + Vector3.forward * rz;
-            if (direction.sqrMagnitude > 0)
-            {
-                transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
-            }
+            Vector3 pointToLook = ray.GetPoint(raylength);
+            Debug.DrawLine(ray.origin, pointToLook, Color.magenta);
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
+        float rx = Input.GetAxis("HorizontalR");
+        float rz = Input.GetAxis("VerticalR");
+        Vector3 direction = Vector3.right * rx + Vector3.forward * rz;
+        if (direction.sqrMagnitude > 0)
+        {
+            transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
         }
     }
 		
