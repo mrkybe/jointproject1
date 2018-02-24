294bd5c1b04fb1008ad42e2a97888b6053b3910c
Alan Tsai <ayt32@drexel.edu>
2018-02-24 14:59:07 -0500

    ShipFactory subclass

---
+++ Assets/Scripts/Classes/Helper/ShipInternals/ShipFactory.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Assets.Scripts.Classes.Helper.ShipInternals;
+
+namespace Assets.Scripts.Classes.Helper.ShipInternals 
+{
+	public class ShipFactory:Building
+	{
+		public ShipFactory(string name, List<CargoItem> consume, List<CargoItem> produce, List<CargoItem> cost): base (name, consume, produce, cost)
+		{
+		}
+	}
+}
+
+
