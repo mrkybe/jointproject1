9af06dda5b408fb67a651ae8f8f4ca2cd057c510
ztcric <tz85@drexel.edu>
2018-03-11 17:47:16 -0400

    Enemy Following Player

--- Assets/Scripts/CombatScripts/AI_Enemy.cs
+++ Assets/Scripts/CombatScripts/AI_Enemy.cs
@@ -14,67 +14,89 @@ public class AI_Enemy : MonoBehaviour {
 	public int kineticDMG = 2;
 	public int laserDMG = 3;
 	public int rocketDMG = 4;
+	public int radius = 20;
+
+	public int speed = 10;
 
 
 	private float x;
 	private float z;
 	private float tiempo;
 	private float angulo;
+	private float dist;
 
+	private int count = 0 ;
+
+
+	public GameObject Player;
 
 	// Use this for initialization
 	void Start () {
-
-
 		x = Random.Range(-velocidadMax, velocidadMax);
 		z = Random.Range(-velocidadMax, velocidadMax);
 		angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
 		transform.localRotation = Quaternion.Euler( 0, angulo, 0);
+		dist = Vector3.Distance (Player.transform.position, gameObject.transform.position);
+
+
+		Debug.DrawLine (transform.position, transform.forward);
 
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-		tiempo += Time.deltaTime;
-
-		if (transform.localPosition.x > xMax) {
-			x = Random.Range(-velocidadMax, 0.0f);
-			angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
-			transform.localRotation = Quaternion.Euler(0, angulo, 0);
-			tiempo = 0.0f; 
+		if (count == 0) {
+			Player = GameObject.FindGameObjectWithTag("Player");
+			count++;
 		}
-		if (transform.localPosition.x < xMin) {
-			x = Random.Range(0.0f, velocidadMax);
-			angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
-			transform.localRotation = Quaternion.Euler(0, angulo, 0); 
-			tiempo = 0.0f; 
+		Debug.Log (Player.transform.position);
+		dist = Vector3.Distance (Player.transform.position, gameObject.transform.position);
+		float step = speed * Time.deltaTime;
+		if (dist < radius) {
+			gameObject.transform.LookAt (Player.transform);
+			gameObject.transform.position = Vector3.MoveTowards (gameObject.transform.position, Player.transform.position, step);
+		} else {
+
+			tiempo += Time.deltaTime;
+
+			if (transform.localPosition.x > xMax) {
+				x = Random.Range (-velocidadMax, 0.0f);
+				angulo = Mathf.Atan2 (x, z) * (180 / 3.141592f) + 90;
+				transform.localRotation = Quaternion.Euler (0, angulo, 0);
+				tiempo = 0.0f; 
+			}
+			if (transform.localPosition.x < xMin) {
+				x = Random.Range (0.0f, velocidadMax);
+				angulo = Mathf.Atan2 (x, z) * (180 / 3.141592f) + 90;
+				transform.localRotation = Quaternion.Euler (0, angulo, 0); 
+				tiempo = 0.0f; 
+			}
+			if (transform.localPosition.z > zMax) {
+				z = Random.Range (-velocidadMax, 0.0f);
+				angulo = Mathf.Atan2 (x, z) * (180 / 3.141592f) + 90;
+				transform.localRotation = Quaternion.Euler (0, angulo, 0); 
+				tiempo = 0.0f; 
+			}
+			if (transform.localPosition.z < zMin) {
+				z = Random.Range (0.0f, velocidadMax);
+				angulo = Mathf.Atan2 (x, z) * (180 / 3.141592f) + 90;
+				transform.localRotation = Quaternion.Euler (0, angulo, 0);
+				tiempo = 0.0f; 
+			}
+
+
+			if (tiempo > 1.0f) {
+				x = Random.Range (-velocidadMax, velocidadMax);
+				z = Random.Range (-velocidadMax, velocidadMax);
+				angulo = Mathf.Atan2 (x, z) * (180 / 3.141592f) + 90;
+				transform.localRotation = Quaternion.Euler (0, angulo, 0);
+				tiempo = 0.0f;
+			}
+
+			transform.localPosition = new Vector3 (transform.localPosition.x + x, transform.localPosition.y, transform.localPosition.z + z);
+
 		}
-		if (transform.localPosition.z > zMax) {
-			z = Random.Range(-velocidadMax, 0.0f);
-			angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
-			transform.localRotation = Quaternion.Euler(0, angulo, 0); 
-			tiempo = 0.0f; 
-		}
-		if (transform.localPosition.z < zMin) {
-			z = Random.Range(0.0f, velocidadMax);
-			angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
-			transform.localRotation = Quaternion.Euler(0, angulo, 0);
-			tiempo = 0.0f; 
-		}
-
-
-		if (tiempo > 1.0f) {
-			x = Random.Range(-velocidadMax, velocidadMax);
-			z = Random.Range(-velocidadMax, velocidadMax);
-			angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
-			transform.localRotation = Quaternion.Euler(0, angulo, 0);
-			tiempo = 0.0f;
-		}
-
-		transform.localPosition = new Vector3(transform.localPosition.x + x, transform.localPosition.y, transform.localPosition.z + z);
-
-
 
 	}
 
