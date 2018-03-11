58a92779546ead78b33199846f62d6adf16abe89
ztcric <tz85@drexel.edu>
2018-03-11 16:17:05 -0400

    Dmg for rocket and laser, second enemy

--- Assets/Scripts/CombatScripts/RocketMove.cs
+++ Assets/Scripts/CombatScripts/RocketMove.cs
@@ -20,15 +20,15 @@ public class RocketMove : MonoBehaviour {
 
 	void OnCollisionEnter(Collision col)
 	{
-		//ps.Play();
-		//Destroy(gameObject,.5f);
-		if (col.gameObject.CompareTag ("Enemy")) {
-			ps.Play ();
-			Destroy (gameObject,.5f);
-		} else if (col.gameObject.CompareTag ("Asteroid")) {
-			ps.Play ();
-			Destroy (gameObject, .5f);
-		}
+//		//ps.Play();
+//		//Destroy(gameObject,.5f);
+//		if (col.gameObject.CompareTag ("Enemy")) {
+//			ps.Play ();
+//			Destroy (gameObject,.5f);
+//		} else if (col.gameObject.CompareTag ("Asteroid")) {
+//			ps.Play ();
+//			Destroy (gameObject, .5f);
+//		}
 	}
 
 }
