eb8c96078b96db49a424801d0fa48c2c2caf35b2
Alan Tsai <ayt32@drexel.edu>
2018-03-11 15:27:01 -0400

    put sound back

--- Assets/Scripts/CombatScripts/Rocket.cs
+++ Assets/Scripts/CombatScripts/Rocket.cs
@@ -8,13 +8,22 @@ public class Rocket : MonoBehaviour {
 	public float amount = 3;
 	public float fireRate = 5;
 	public float nextFire;
+	public AudioClip shootSound;
 
+	private AudioSource source;
+
+
+	void Awake(){
+
+		source = GetComponent <AudioSource> ();
+	}
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown ("Fire1") && Time.time > nextFire && amount != 0) 
 		{
 			nextFire = Time.time + fireRate;
 			Instantiate (ammo, transform.position, transform.rotation);
+			source.PlayOneShot (shootSound);
 			amount--;
 		}
 	}
